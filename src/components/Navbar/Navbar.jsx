63f918ac73bf93831ad4a86d762d0f7dc9094a45
unknown <Administrator@KH-1012949162.saglik.lokal>
2025-05-21 11:01:59 +0300

    first commit-21.05.2025

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './navbar.css'
+
+import { TiHome } from 'react-icons/ti'
+import { FaHeart } from 'react-icons/fa'
+import { PiFilmReelFill } from 'react-icons/pi'
+
+
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="left">
+                <h1>Movie App</h1>
+            </div>
+            <div className="center">
+                <PiFilmReelFill />
+            </div>
+            <div className="right">
+                <ul>
+                    <li>
+                        <TiHome />
+                    </li>
+                    <li>
+                        <FaHeart />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
